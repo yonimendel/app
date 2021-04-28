f7c385033392190698670b680d1cfed60f00de80
Yoni <you@example.com>
2021-04-28 22:23:54 +0300

    template

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const PORT = 4567;
+const app = express();
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, ()=> console.log(`listening to server at port ${PORT}`));
